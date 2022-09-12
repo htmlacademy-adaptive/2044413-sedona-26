e06303f13d838ddeb34c286828a06d0d4626ba5a
Keks <keks@htmlacademy.ru>
2022-09-13 02:12:32 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/script.js
+++ 8/js/script.js
@@ -1,14 +1,1 @@
-let navPage = document.querySelector('.page-navigation');
-let navToggle = document.querySelector('.page-navigation__toggle');
-
-navPage.classList.remove('page-navigation--nojs');
-
-navToggle.addEventListener('click', function () {
-  if (navPage.classList.contains('page-navigation--closed')) {
-    navPage.classList.remove('page-navigation--closed');
-    navPage.classList.add('page-navigation--opened');
-  } else {
-    navPage.classList.add('page-navigation--closed');
-    navPage.classList.remove('page-navigation--opened');
-  }
-});
+let navPage=document.querySelector(".page-navigation"),navToggle=document.querySelector(".page-navigation__toggle");navPage.classList.remove("page-navigation--nojs"),navToggle.addEventListener("click",(function(){navPage.classList.contains("page-navigation--closed")?(navPage.classList.remove("page-navigation--closed"),navPage.classList.add("page-navigation--opened")):(navPage.classList.add("page-navigation--closed"),navPage.classList.remove("page-navigation--opened"))}));
